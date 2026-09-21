e3632b802f832f16e026b4448368c6d2c9aa1d24
jjunseokk <p.junseok53@gmail.com>
2026-09-21 17:39:03 +0900

    [fix] 에러수정

--- src/util/AxiosConfig.tsx
+++ src/util/AxiosConfig.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const AxiosConfig = axios.create({
-  baseURL: 'http://ec2-15-165-137-59.ap-northeast-2.compute.amazonaws.com/api',
+  baseURL: 'http://localhost:8080/api/',
+  // baseURL: 'http://ec2-15-165-137-59.ap-northeast-2.compute.amazonaws.com/api',
   withCredentials: true,
 });
 
